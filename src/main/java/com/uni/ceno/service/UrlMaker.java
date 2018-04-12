e64dfb0cf32235ffc10865428d362bd44c4e6131
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2018-04-12 12:07:54 +0430

    Change shareWebsites to SocialWebsite enum

--- src/main/java/com/uni/ceno/service/UrlMaker.java
+++ src/main/java/com/uni/ceno/service/UrlMaker.java
@@ -13,22 +13,37 @@ import java.util.regex.Pattern;
 public class UrlMaker {
 
     private static final Pattern URL_PATTERN = Pattern.compile("[^0-9a-zA-Z$\\-_.+!*'()]");
-    private static final String FACEBOOK_SHR = "http://facebook.com/sharer/sharer.php?u=";
-    private static final String TWITTER_SHR = "http://twitter.com/home?status=";
-    private static final String TUMBLR_SHR = "http://tumblr.com/widgets/share/tool?canonicalUrl=";
-    private static final String PINTEREST_SHR = "http://pinterest.com/pin/create/button/?url=";
 
     public String makeUrlOf(String base) {
-        String rawUrl = base + LocalDate.now() + LocalTime.now().hashCode();
+        String rawUrl = String.format("%s-%s%d", base, LocalDate.now(), LocalTime.now().hashCode());
+        rawUrl = rawUrl.toLowerCase();
         return URL_PATTERN.matcher(rawUrl).replaceAll("-");
     }
 
-    public List<Post.ShareUrl> makeShareUrlsFor(String postUrl) {
+    public List<Post.ShareUrl> makeShareUrlsFor(String url) {
         List<Post.ShareUrl> shareUrls = new ArrayList<>();
-        shareUrls.add(new Post.ShareUrl("facebook", FACEBOOK_SHR + postUrl));
-        shareUrls.add(new Post.ShareUrl("twitter", TWITTER_SHR + postUrl));
-        shareUrls.add(new Post.ShareUrl("pinterest", PINTEREST_SHR + postUrl));
-        shareUrls.add(new Post.ShareUrl("tumblr", TUMBLR_SHR + postUrl));
+        for (SocialWebsite socialWebsite : SocialWebsite.values()) {
+            shareUrls.add(new Post.ShareUrl(socialWebsite.name(),
+                    socialWebsite.getShareUrl() + url));
+        }
         return shareUrls;
+    }
+
+    private enum SocialWebsite {
+
+        FACEBOOK("http://facebook.com/sharer/sharer.php?u="),
+        TWITTER("http://twitter.com/home?status="),
+        TUMBLR("http://tumblr.com/widgets/share/tool?canonicalUrl="),
+        PINTEREST("http://pinterest.com/pin/create/button/?shareUrl=");
+
+        private final String shareUrl;
+
+        SocialWebsite(String shareUrl) {
+            this.shareUrl = shareUrl;
+        }
+
+        public String getShareUrl() {
+            return shareUrl;
+        }
     }
 }
