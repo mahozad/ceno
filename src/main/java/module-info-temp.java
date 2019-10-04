2cd567b28bd6cb33eaeb1f18e7bf6e3fc2fffb1b
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-10-04 12:14:10 +0330

    Add a template module-info file

---
+++ src/main/java/module-info-temp.java
@@ -0,0 +1,32 @@
+/*
+module ir.ceno.main {
+    requires java.compiler;
+    requires java.desktop;
+    requires java.persistence;
+    requires java.validation;
+    requires aspectjweaver;
+    requires com.github.benmanes.caffeine;
+    requires com.rometools.rome;
+    requires imgscalr.lib;
+    requires logback.classic;
+    requires lucene.misc;
+    requires org.hibernate.orm.core;
+    requires org.hibernate.search.engine;
+    requires org.hibernate.search.orm;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.context.support;
+    requires spring.core;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.security.config;
+    requires spring.security.core;
+    requires spring.security.web;
+    requires spring.tx;
+    requires spring.web;
+    requires spring.webmvc;
+    requires tomcat.embed.core;
+}
+*/
